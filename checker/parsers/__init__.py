471428039686661cb5cbc23da66f57eb86d2ce21
iliefu <105096542+iliefu@users.noreply.github.com>
2026-06-10 13:40:27 +0800

    Create __init__.py

---
+++ checker/parsers/__init__.py
@@ -0,0 +1,10 @@
+from . import base
+from . import vmess
+from . import vless
+from . import trojan
+from . import ss
+from . import hysteria2
+from . import tuic
+from . import reality
+
+__all__ = ['base', 'vmess', 'vless', 'trojan', 'ss', 'hysteria2', 'tuic', 'reality']
